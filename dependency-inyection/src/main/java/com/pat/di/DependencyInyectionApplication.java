f4a10e7a1f4f400fc0ab8f7a75efb9553ce1d7db
Patricio104 <patriciojuarez104@gmail.com>
2024-04-04 13:02:55 -0600

    Uso de la anotación @Qualifier() cuando se usa autowired

--- dependency-inyection/src/main/java/com/pat/di/DependencyInyectionApplication.java
+++ dependency-inyection/src/main/java/com/pat/di/DependencyInyectionApplication.java
@@ -1,19 +1,31 @@
 package com.pat.di;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.pat.di.atributo.Coche;
 import com.pat.di.atributo.Motor;
+import com.pat.di.qualifiers.Animal;
+import com.pat.di.qualifiers.Nido;
+import com.pat.di.qualifiers.Pajaro;
+import com.pat.di.qualifiers.Perro;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
+
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
-		Coche coche = context.getBean(Coche.class);
-		System.out.println(coche);
+		//Perro perro = context.getBean(Perro.class);
+		//Animal animal = context.getBean("perrito", Animal.class);
+		//log.info("Objeto animal, nombre: {} edad: {}", animal.getNombre(), animal.getEdad());
+		Nido nido = context.getBean(Nido.class);
+		nido.imprimir();
 	}
 
 }
