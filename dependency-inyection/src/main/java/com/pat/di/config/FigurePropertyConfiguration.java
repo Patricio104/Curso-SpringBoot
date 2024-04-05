21bb5dc3e50d56f2e77b60f394cfba03b0b368bc
Patricio104 <patriciojuarez104@gmail.com>
2024-04-04 19:09:18 -0600

    Carga de propiedades desde un archivo, usando la siguiente forma para mandar a traer los valores (.radius:1) el valor despúes de los dos puntos es para setear el dato por defecto si no es informado en el archivo properties

---
+++ dependency-inyection/src/main/java/com/pat/di/config/FigurePropertyConfiguration.java
@@ -0,0 +1,14 @@
+package com.pat.di.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:areas.properties")
+public class FigurePropertyConfiguration {
+
+	public PropertySourcesPlaceholderConfigurer getPropertiesConfig() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
